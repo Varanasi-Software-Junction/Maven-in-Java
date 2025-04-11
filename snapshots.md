8f600d6dcdc2a174412f8f6878983e1de73543db
champaksworldcreate <champaksworld@gmail.com>
2025-04-11 11:05:51 +0530

    Face recognition pics

---
+++ snapshots.md
@@ -0,0 +1,4 @@
+# Face Recognition
+
+![alt text](faces.png) ![alt text](out.png) ![alt text](out3.png) ![alt text](three.png) ![alt text](write.png)
+ 
--- src/main/java/mycopencv/TestOpenCV.java
+++ src/main/java/mycopencv/TestOpenCV.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mycopencv;
+
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
@@ -13,48 +14,49 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 public class TestOpenCV {
-public static Mat loadImage(String imagePath) {
-  Imgcodecs imgcodecs=  new Imgcodecs();
-  Mat matrix =imgcodecs.imread(imagePath);
-    return matrix;
-}
 
-public static void saveImage(Mat imageMatrix, String targetPath) {
-    Imgcodecs.imwrite(targetPath, imageMatrix);
-}
-public static void faceDetect(Mat input)
-{
-    MatOfRect facesDetected = new MatOfRect();
-    
-     
-CascadeClassifier classifier = new CascadeClassifier("xml/facedetect.xml");
-MatOfRect faceDetections = new MatOfRect(); 
-classifier.detectMultiScale(input, faceDetections);
- System.out.println(String.format("Detected %s faces", 
-         faceDetections.toArray().length));
-    
- 
- 
- for (Rect rect : faceDetections.toArray()) {
-         Imgproc.rectangle(
-            input,                                               // where to draw the box
-            new Point(rect.x, rect.y),                            // bottom left
-            new Point(rect.x + rect.width, rect.y + rect.height), // top right
-            new Scalar(0, 0, 255),
-            3                                                     // RGB colour
-         );
-      }
-    saveImage(input, "images/out.png");
-    
-}
+    public static Mat loadImage(String imagePath) {
+        Imgcodecs imgcodecs = new Imgcodecs();
+        imagePath = String.format("images/%s", imagePath);
+        Mat matrix = imgcodecs.imread(imagePath);
+        return matrix;
+    }
+
+    public static void saveImage(Mat imageMatrix, String targetPath) {
+        targetPath = String.format("images/%s", targetPath);
+        Imgcodecs.imwrite(targetPath, imageMatrix);
+    }
+
+    public static void faceDetect(Mat input) {
+
+        CascadeClassifier classifier = new CascadeClassifier("xml/facedetect.xml");
+        MatOfRect faceDetections = new MatOfRect();
+        classifier.detectMultiScale(input, faceDetections);
+        System.out.println(String.format("Detected %s faces",
+                faceDetections.toArray().length));
+
+        for (Rect rect : faceDetections.toArray()) {
+            Imgproc.rectangle(
+                    input, // where to draw the box
+                    new Point(rect.x, rect.y), // bottom left
+                    new Point(rect.x + rect.width, rect.y + rect.height), // top right
+                    new Scalar(0, 0, 255),
+                    3 // RGB colour
+            );
+        }
+        System.out.println("Saving");
+        saveImage(input, "out3.png");
+
+    }
+
     public static void main(String[] args) {
 //        OpenCV.loadShared();
-nu.pattern.OpenCV.loadLocally();
+        nu.pattern.OpenCV.loadLocally();
         System.out.println("Hello Open CV");
-        Mat m1=loadImage("images/faces.png");
+        Mat m1 = loadImage("three.png");
         System.out.println(m1);
         faceDetect(m1);
-        saveImage(m1, "images/write.png");
+
     }
 
 }
